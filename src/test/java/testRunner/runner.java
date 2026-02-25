4b406826c7ad2e7e1cff3cb8d6a8dff1c969bccd
pranavsuryawanshi96 <pranavsuryawanshi61@gmail.com>
2026-02-25 18:09:36 +0530

    fixed runer file

--- src/test/java/testRunner/runner.java
+++ src/test/java/testRunner/runner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		features = "src\\test\\resources\\features\\form.feature",
+		features = "src/test/resources/features/form.feature",
 		  glue="stepDefination", dryRun = false,
 		  plugin = { "pretty","html:target/cucumber-reports/cucumber.html",
 				  
